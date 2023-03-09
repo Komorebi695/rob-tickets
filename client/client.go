7fedc5539a94e8ce0baac6568e91161db1e9b009
niexiaojiang <2570040683@qq.com>
2023-03-09 11:08:46 +0800

    版本1.0完成。

---
+++ client/client.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+const TokenExpireDuration = time.Hour * 24
+
+var mySecret = []byte("秘密")
+var cli = &http.Client{}
+var failCount float32
+
+type myClaims struct {
+	UserID               string `json:"user_id"`
+	jwt.RegisteredClaims        // 内嵌申明字段
+}
+
+func main() {
+	initClient()
+	failCount = 0
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	beginTime := time.Now()
+	go doBuy(0, 40000, wg)
+	//go doBuy(40001, 60000, wg)
+	wg.Wait()
+	endTime := time.Now()
+	fmt.Printf("请求失败率：%f%%\n耗时: %v \n失败次数: %d", failCount*100/float32(40000), endTime.Sub(beginTime), int(failCount))
+}
+
+func initClient() {
+	tr := &http.Transport{
+		Proxy:           http.ProxyFromEnvironment,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:        10,                // 空闲(保持活跃)连接的最大数量
+		MaxConnsPerHost:     5000,              // 限制每台主机的连接总数
+		MaxIdleConnsPerHost: 5000,              // 保持的最大空闲连接数
+		IdleConnTimeout:     300 * time.Second, // 空闲连接(保持连接)在关闭自己之前保持空闲的最大时间
+	}
+	cli = &http.Client{
+		Transport: tr,
+		Timeout:   9 * time.Second,
+	}
+}
+
+func doBuy(begin int, end int, group *sync.WaitGroup) {
+	defer group.Done()
+	// 同步组
+	g := &sync.WaitGroup{}
+	g.Add(end - begin + 1)
+	// 并发请求
+	for i := begin; i <= end; i++ {
+		token, err := GenToken(strconv.Itoa(i))
+		if err != nil {
+			panic("生成token失败：" + err.Error())
+		}
+		time.Sleep(time.Nanosecond)
+		go func(*sync.WaitGroup, string) {
+			if err := sendRequest(g, token); err != nil {
+				l := &sync.Mutex{}
+				l.Lock()
+				failCount++
+				l.Unlock()
+				fmt.Println("请求出错: ", err.Error())
+			}
+		}(g, token)
+	}
+	g.Wait()
+}
+
+func sendRequest(wg *sync.WaitGroup, token string) error {
+	// 1. 设置请求参数
+	params := url.Values{}
+	params.Set("token", token)
+	params.Set("ticket_id", "h1k4J7Dyt0")
+	params.Set("count", "1")
+	// 2. 伪造请求
+	req, err := http.NewRequest("POST", "http://localhost:8080/buy", strings.NewReader(params.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(params.Encode())))
+	// 3. 发送请求
+	resp, err := cli.Do(req)
+	defer wg.Done()
+	if err != nil {
+		return err
+	}
+	_, _ = ioutil.ReadAll(resp.Body)
+	return nil
+}
+
+// GenToken 生成JWT
+func GenToken(userID string) (string, error) {
+	// 创建一个我们自己的声明
+	claims := myClaims{
+		userID,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TokenExpireDuration)),
+			Issuer:    "my-project", // 签发人
+		},
+	}
+	// 使用指定的签名方法创建签名对象
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// 使用指定的secret签名并获得完整的编码后的字符串token
+	return token.SignedString(mySecret)
+}
---
+++ src/dao/mysql/mysql.go
@@ -0,0 +1,48 @@
+package mysql
+
+import (
+	"fmt"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"robTickets/src/configs"
+	"time"
+)
+
+var db *gorm.DB
+
+// InitMysql 初始化MySQL
+func InitMysql(initConfig *configs.AppConfig) (err error) {
+	// 获取配置信息
+	database := initConfig.DataBase
+
+	// 日志
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Warn, // 日志级别
+			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,        // 禁用彩色打印
+		},
+	)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", database.User, database.Pwd, database.Host, database.Port, database.Database)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		return
+	}
+
+	// 迁移
+	err = db.AutoMigrate()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
